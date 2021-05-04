067b9b4ed71a29f9dd5167eccc21e2ef47e6d27e
Tom Dyson <tom@torchbox.com>
2021-05-04 17:08:36 +0100

    Update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,7 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = () => ({
+module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
@@ -17,7 +17,9 @@ module.exports = () => ({
           rejectUnauthorized: false,
         },
       },
-      options: {},
+      options: {
+        ssl: true,
+      },
     },
   },
 });
