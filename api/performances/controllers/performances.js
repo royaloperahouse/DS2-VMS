9b46fbe5b308f44bba307fec4bcca699dac48bd3
Vasyl Lenko <vasyl.lenko@roh.org.uk>
2021-06-25 12:59:19 +0300

    Prismic API contract is implemented

--- api/performances/controllers/performances.js
+++ api/performances/controllers/performances.js
@@ -1,8 +1,26 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
 
-module.exports = {};
+  /**
+   * Retrieve Perfromances conforming to the requirements of Prismic API.
+   *
+   * @return {Array}
+   */
+  async find(ctx) {
+    let entities = await strapi.services.performances[ctx.query._q ? "search" : "find"](ctx.query);
+    entities = entities.map(entity => sanitizeEntity(entity, { model: strapi.models.performances }));
+    return {
+      results_size: entities.length,
+      results: entities.map(entity => ({
+        id: entity.diese_activity_id,
+        title: entity.diese_activity_title,
+        description: "Digital Performance",
+        image_url: "https://static.roh.org.uk/redesign/Logo_detail.png",
+        last_update: Date.parse(entity.updated_at),
+        blob: entity
+      }))
+    };
+  },
+
+};
