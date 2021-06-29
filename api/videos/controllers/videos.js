f33185d84f26e922f483e3dbf0544a917eea3e2b
Vasyl Lenko <vasyl.lenko@roh.org.uk>
2021-06-29 18:18:44 +0300

    Ingestor API contract is implemented

--- api/videos/controllers/videos.js
+++ api/videos/controllers/videos.js
@@ -1,8 +1,19 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
 
-module.exports = {};
+    /**
+     * Creates a Video record and associates it with the relevant Performace.
+     *
+     * @return {Object}
+     */
+    async create(ctx) {
+        const { diese_activity_id } = ctx.request.body;
+        let performance = await strapi.services.performances.findOne({ diese_activity_id });
+        if (!performance) performance = await strapi.services.performances.create({ diese_activity_id });
+
+        const video = await strapi.services.videos.create({ ...ctx.request.body, performance });
+        return sanitizeEntity(video, { model: strapi.models.videos });
+    },
+
+};
